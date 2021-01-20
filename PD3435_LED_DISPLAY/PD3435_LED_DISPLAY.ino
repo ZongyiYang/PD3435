835009cac3ebb6c6da825c6ddee3ccd61576dc5b
ZongyiYang <yangzongyi@hotmail.com>
2021-01-20 18:54:57 -0500

    Add data bus read test to demo

--- PD3435_LED_DISPLAY/PD3435_LED_DISPLAY.ino
+++ PD3435_LED_DISPLAY/PD3435_LED_DISPLAY.ino
@@ -5,21 +5,23 @@
 
 namespace PD3435
 {
+  const byte _RD  = A4; // read
   const byte _WR  = A3; // write
   const byte _RST = A0; // reset
   const byte CE1  = A1; // chip enables
   const byte _CE0 = A2;
-  const byte A2   = 10; // address inputs
-  const byte A1   = 9;
-  const byte A0   = 8;
-  const byte D0   = 0;  // data bus I/O
-  const byte D1   = 1;
-  const byte D2   = 2;
-  const byte D3   = 3;
-  const byte D4   = 4;
-  const byte D5   = 5;
-  const byte D6   = 6;
-  const byte D7   = 7;
+  const byte A2   = 12; // address inputs
+  const byte A1   = 11;
+  const byte A0   = 10;
+  const byte D0   = 2;  // data bus I/O
+  const byte D1   = 3;
+  const byte D2   = 4;
+  const byte D3   = 5;
+  const byte D4   = 6;
+  const byte D5   = 7;
+  const byte D6   = 8;
+  const byte D7   = 9;
+  // pins 0 and 1 are used for Serial com in this demo
   
   void writeAddress(int addr)
   {
@@ -39,9 +41,34 @@ namespace PD3435
     digitalWrite(D6, bitRead(data, 6));
     digitalWrite(D7, bitRead(data, 7));
   }
+
+  int readData()
+  {
+    int data = 0;
+    data |= digitalRead(D7)<<7;
+    data |= digitalRead(D6)<<6;
+    data |= digitalRead(D5)<<5;
+    data |= digitalRead(D4)<<4;
+    data |= digitalRead(D3)<<3;
+    data |= digitalRead(D2)<<2;
+    data |= digitalRead(D1)<<1;
+    data |= digitalRead(D0);
+    return data;
+  }
   
   void doWrite(int addr, int data)
   {
+    digitalWrite(_RD, HIGH);
+    digitalWrite(_WR, HIGH);
+    pinMode(D0, OUTPUT);
+    pinMode(D1, OUTPUT);
+    pinMode(D2, OUTPUT);
+    pinMode(D3, OUTPUT);
+    pinMode(D4, OUTPUT);
+    pinMode(D5, OUTPUT);
+    pinMode(D6, OUTPUT);
+    pinMode(D7, OUTPUT);
+    
     digitalWrite(_CE0, LOW);
     digitalWrite(CE1, HIGH);
     digitalWrite(_WR, HIGH);
@@ -52,10 +79,35 @@ namespace PD3435
     digitalWrite(_WR, LOW);
     digitalWrite(_WR, HIGH);
   }
+
+  int doRead(int addr)
+  {
+    digitalWrite(_RD, HIGH);
+    digitalWrite(_WR, HIGH);
+    pinMode(D0, INPUT);
+    pinMode(D1, INPUT);
+    pinMode(D2, INPUT);
+    pinMode(D3, INPUT);
+    pinMode(D4, INPUT);
+    pinMode(D5, INPUT);
+    pinMode(D6, INPUT);
+    pinMode(D7, INPUT);
+    
+    digitalWrite(_CE0, LOW);
+    digitalWrite(CE1, HIGH);
+  
+    writeAddress(addr);
+    
+    digitalWrite(_RD, LOW);
+    int data = readData();
+    digitalWrite(_RD, HIGH);
+
+    return data;
+  }
   
   // lamptest overwrites all other attributes
   // blink applies to all digits
-  void setAttribute(bool doLampTest, bool doBlink, int attr, int brightness)
+  int setAttribute(bool doLampTest, bool doBlink, int attr, int brightness)
   {
     unsigned int data = 0x00;
     data |= (brightness & B11);
@@ -63,6 +115,7 @@ namespace PD3435
     data |= (doBlink << 5);
     data |= (doLampTest << 6);
     doWrite(0x00, data);
+    return data;
   }
   
   // digit 0 is rightmost digit
@@ -73,10 +126,12 @@ namespace PD3435
 
   void init()
   {
+    digitalWrite(_RD, HIGH);
     digitalWrite(CE1, HIGH);
     digitalWrite(_CE0, HIGH);
     digitalWrite(_RST, HIGH);
     digitalWrite(_WR, HIGH);
+    pinMode(_RD, OUTPUT);
     pinMode(CE1, OUTPUT);
     pinMode(_CE0, OUTPUT);
     pinMode(_RST, OUTPUT);
@@ -97,12 +152,27 @@ namespace PD3435
 
 void setup()
 {
+  Serial.begin(9600);
+  
   PD3435::init();
+  int ret;
   
   // do a lamp test for 1s to start
-  PD3435::setAttribute(true, false, 0, 0);
+  Serial.println("Doing lamp test");
+  ret = PD3435::setAttribute(true, false, 0, 0);
+  Serial.print("Wrote: ");
+  Serial.println(ret, BIN);
   delay(1000);
-  PD3435::setAttribute(false, false, B101, 1);
+  
+  Serial.println("Do read on address 0:");
+  Serial.println(PD3435::doRead(0x00), BIN);
+  
+  Serial.println("Set attributes");
+  ret = PD3435::setAttribute(false, false, B101, 1);
+  Serial.print("Wrote: ");
+  Serial.println(ret, BIN);
+  Serial.println("Read attributes:");
+  Serial.println(PD3435::doRead(0x00), BIN);
 }
 
 const int digits = 4;
